a8d054d85db500adc48a2795a5ac7775c290f299
IsabelleCristine1107 <Isabellecristinesesipaulista@gmail.com>
2024-08-28 17:45:14 -0300

    Add files via upload

---
+++ 01-aula.py
@@ -0,0 +1,19 @@
+print("Isabelle")
+print("Cristine")
+print("Miranda")
+print("Isabelle Cristine Miranda")
+print("Isabelle 'Belle' Cristine")
+print("Isabelle \"Belle\" Critsine")
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))

c7680acb56e8132b9aeabd33d32ac806972d34b4
IsabelleCristine1107 <Isabellecristinesesipaulista@gmail.com>
2024-10-14 15:06:18 -0300

    Add files via upload

--- 19-conversor-temperatura.py
+++ 19-conversor-temperatura.py
@@ -3,27 +3,27 @@
 
 def Farenheit_para_Kelvin(F):
     K = (F-32)*5/9+273.15
-    return (K)
+    return K
 
 def Farenheit_para_Celsius(F):
     C = (F-32)*5/9
-    return (C)
+    return C
 
 def Kelvin_para_Farenheit(K):
     F = (K-273.15)*9/5+32
-    return (F)
+    return F
 
 def Kelvin_para_Celsius(K):
     C = K-273.15
-    return (C)
+    return C
 
 def Celsius_para_Farenheit(C):
     F = c*9/5+32
-    return (F)
+    return F
 
 def Celsius_para_Kelvin(C):
     K = C+273.15
-    return (K)
+    return K
 
 def Menu():
     print("Escolha a opção desejada:")
@@ -37,27 +37,53 @@ def Menu():
 
 while True:
     Menu()
-    opcao =  input("Escolha a opção: ")
+    opcao = input("Escolha a opção: ")
     
-    if opcao == 1:
-        F = float(input("Digite a temperatura em Farenheit: "))
-        K =  Farenheit_para_Kelvin(F)
-        print(f"A temperatura em Kelvin é: {K} K")
-    elif opcao  == 2:
-        F = float(input("Digite a temperatura em Farenheit: "))
-        C = Farenheit_para_Celsius(F)
-        print(f"A temperatura em Celsius é: {C} C")
-    elif opcao == 3:
-        K  = float(input("Digite a temperatura em Kelvin: "))
-        F = Kelvin_para_Farenheit(K)
-        print(f"A temperatura em Farenheit é: {F} F")
-    elif opcao  == 4:
-        K = float(input("Digite a temperatura em Kelvin: "))
-        C = Kelvin_para_Celsius(K)
-        print(f"A temperatura em Celsius é: {C} C")
-    elif opcao == 5:
+    if opcao == "1":
+        try:
+            F = float(input("Digite a temperatura em Farenheit: "))
+            K =  Farenheit_para_Kelvin(F)
+            print(f"A temperatura em Kelvin é: {K} K")
+        except ValueError:
+            print("Erro: valor inválido. Tente novamente.")
+    elif opcao  == "2":
+        try:    
+            F = float(input("Digite a temperatura em Farenheit: "))
+            C = Farenheit_para_Celsius(F)
+            print(f"A temperatura em Celsius é: {C} C")
+        except ValueError:
+            print("Erro: valor inválido. Tente novamente.")
+    elif opcao == "3":
+        try:
+            K  = float(input("Digite a temperatura em Kelvin: "))
+            F = Kelvin_para_Farenheit(K)
+            print(f"A temperatura em Farenheit é: {F} F")
+        except ValueError:
+            print("Erro: Valor inválido. Tente novamente.")
+    elif opcao  == "4":
+        try:
+            K = float(input("Digite a temperatura em Kelvin: "))
+            C = Kelvin_para_Celsius(K)
+            print(f"A temperatura em Celsius é: {C} C")
+        except ValueError:
+            print("Erro: Valor inválido. Tente Novamente.")
+    elif opcao == "5":
+        try:
+            C = float(input("Digite a temperatura em Celsius: "))
+            F = Celsius_para_Farenheit(C)
+            print(f"A temperatura em Farenheit é: {F} F")
+        except ValueError:
+            print("Erro: valor inválido. Tente novamente.")
+    elif opcao == "6":
+        try:
+            C = float(input("Digite a temperatura em Celsius: "))
+            K = Celsius_para_Kelvin(C)
+            print(f"A temperatura em Kelvin é: {K} K")
+        except ValueError:
+            print("Erro: valor inválido. Tente novamente.")
+    elif opcao == "7":
+        print("Saindo do programa...")
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
         
-
-
-
-
